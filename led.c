79737a22e8ff185a85e7e0c84a02354f2e58a913
MatrixMike <matroxmike@icloud.com>
2016-10-08 11:38:46 +1100

    format by indent

--- led.c
+++ led.c
@@ -1,49 +1,26 @@
 //filename: led.c：
-
-
+// scraped from screen 
+// https://www.element14.com/community/docs/DOC-55219/l/how-to-program-embedded-pi-using-raspberry-pi
+// starting building application 
+// Mike Hewitt  08.10.2016 09:10:14
 #include "reg.h"
 
-
 void delay_ms(unsigned short nms)
-
-
 {
-
-       unsigned long i;
-
-       while(nms--)
-
-       for(i = 0; i < 1000; i++);
-
+	unsigned long i;
+	while (nms--)
+		for (i = 0; i < 1000; i++) ;
 }
 
-
 int main(void)
-
-
 {
-
-       RCC_APB2ENR |= (1<<3);
-
-       GPIOB_CRH = (2<<20) | (0<<22);
-
-       GPIOB_ODR = 1<<13;   //PB13
-
-
-    while(1)
-
-
-    {
-
-       GPIOB_ODR |= 1<<13;
-
-       delay_ms(500);
-
-       GPIOB_ODR &= ~(1<<13);
-
-       delay_ms(500);
-
-    }
-
-
+	RCC_APB2ENR |= (1 << 3);
+	GPIOB_CRH = (2 << 20) | (0 << 22);
+	GPIOB_ODR = 1 << 13;	//PB13
+	while (1) {
+		GPIOB_ODR |= 1 << 13;
+		delay_ms(500);
+		GPIOB_ODR &= ~(1 << 13);
+		delay_ms(500);
+	}
 }
--- reg.h
+++ reg.h
@@ -1,25 +1,16 @@
 //filename: reg.h:
 
-
 #ifndef _REGS_H_
-
-
 #define _REGS_H_
-
 
 #define RCC_APB2ENR *(volatile unsigned long *)0x40021018
 
-
 #define GPIOB_CRL *(volatile unsigned long *)0x40010C00
-
 
 #define GPIOB_CRH *(volatile unsigned long *)0x40010C04
 
-
 #define GPIOB_IDR *(volatile unsigned long *)0x40010C08
 
-
 #define GPIOB_ODR *(volatile unsigned long *)0x40010C0C
-
 
 #endif
