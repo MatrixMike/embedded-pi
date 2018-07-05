80b0d640229e04e7dffa37489eed456da8f2275c
mike hewitt <mikeh@electroteach.com>
2018-07-05 12:27:49 +1000

    made into a shell file

--- makeall.sh
+++ makeall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 arm-none-eabi-gcc -Wall -g -O0 -nostartfiles    -c -o led.o led.c
 arm-none-eabi-gcc -Wall -g -O0 -nostartfiles    -c -o startup_coide.o startup_coide.c
 arm-none-eabi-ld -Tstm32_f103_gcc.ld -o led.elf  led.o startup_coide.o 
